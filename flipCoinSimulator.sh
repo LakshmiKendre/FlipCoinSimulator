6f0a3a7b04bfac30b50c65223bc8fbeb3c0f8592
admin <lakshmi.patil.nine@gmail.com>
2020-01-24 15:45:28 +0530

    added: calculated percetage of singlet combinations

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,10 +2,36 @@
 
 echo "Welcome to flip-Coin-Simulator"
 
-if [ $((RANDOM%2)) -eq 0 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+#declaring dictionary
+declare -A Dictionary 
 
+#declaring variables
+H=0
+T=0
+
+# function to flip coin and taking count of heads and tails
+flip_coin()
+{
+read -p "Enter how many time you want to flip coin : " flipCoin
+
+for((i=0;i<flipCoin;i++))
+do
+	if [ $((RANDOM%2)) -eq 0 ]
+	then
+		Dictionary[$i]="Heads" # storing result in dictionary
+		let H++
+	else
+		Dictionary[$i]="Tails"
+		let T++
+	fi
+done
+}
+
+# calling function
+flip_coin
+
+# calculating percentage of heads and tails and print it
+echo "Heads percentage :" $(echo "scale=2; $H*100/$flipCoin " | bc)
+echo "Tails percentage :" $(echo "scale=2; $T*100/$flipCoin " | bc)
+
+echo Number of flips of coin : ${Dictionary[@]}
